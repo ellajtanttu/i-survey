d23fb21b728267b0909220bd4d8a87b932e66f92
Ella Tanttu <ellajtanttu@gmail.com>
2021-09-22 10:45:16 -0700

    add js arrays to display responses in a list (lesson 3.11)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,38 @@
 //Business Logic:
 
+let favoriteThings = [];
+let firstFaves = [];
+let pushArray = []; 
 
 //UI Logic:
 $(document).ready(function() {
-
   $("#persForm").submit(function(event) {
     event.preventDefault();
+
+    favoriteThings = [$("#name").val(), $("#birthday").val(), $("#faveFoods").val(),$("input:radio[name=faveMusic]:checked").val(),document.getElementById("faveColor").value];
+    console.log(favoriteThings);
+    firstFaves = favoriteThings.slice(0,3);
+    console.log(firstFaves);
+    pushArray.push(firstFaves[0]);
+    pushArray.push(firstFaves[1]);
+    pushArray.push(firstFaves[2]);
+    console.log(pushArray);
+
+    const pushArrayOne = pushArray[0];
+    console.log(pushArrayOne);
+    const pushArrayTwo = pushArray[1];
+    console.log(pushArrayTwo);
+    const pushArrayThree = pushArray[2];
+    console.log(pushArrayThree);
+
+    $(".arrayDiv").show(function() {
+      $(".arrayList").append("<li id='pAOne'></li>");
+      $("#pAOne").text(pushArrayOne);
+      $(".arrayList").append("<li id='pATwo'></li>");
+      $("#pATwo").text(pushArrayTwo);
+      $(".arrayList").append("<li id='pAThree'></li>");
+      $("#pAThree").text(pushArrayThree);
+    });
 
     let inputName = $("#name").val();
     let inputBD = $("#birthday").val();
@@ -14,6 +41,8 @@ $(document).ready(function() {
     // let inputColor = $("faveColor").val();
     let inputColor = document.getElementById("faveColor").value;
     // alert(inputColor);
+
+    let nameLength = inputName.length;
 
     $("#userName").text(inputName);
     $("#userBirth").text(inputBD);
@@ -24,6 +53,11 @@ $(document).ready(function() {
     $("#formButton").css("background-color", inputColor);
 
     $(".resultsSumm").show();
+
+    if (nameLength === 0) {
+      $("#labelName").css("background-color" , "red");
+      console.log("name is 0");
+    } 
   });
 
 });
